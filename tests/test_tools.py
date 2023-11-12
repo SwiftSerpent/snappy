21247fc172104fa98a2a30051a31536f68efc0b2
Parker Brown <brownpt98@gmail.com>
2023-11-12 11:37:48 -0600

    Fix test output image location

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -7,21 +7,21 @@ from snappy.tools import Screenshotter
 class TestScreenshotter(unittest.TestCase):
   def test_take_screenshot(self):
     img_format = 'png'
-    screenshotter = Screenshotter(format=img_format)
+    screenshotter = Screenshotter(format=img_format, output_dir='test_images')
     url = 'https://www.google.com'
     filename = 'google'
     screenshotter.take_screenshot(url, filename)
-    self.assertTrue(os.path.exists(filename + '.' + img_format))
+    self.assertTrue(os.path.exists(os.path.join('test_images', filename + '.' + img_format)))
 
   def test_take_screenshot_with_invalid_url(self):
-    screenshotter = Screenshotter()
+    screenshotter = Screenshotter(output_dir='test_images')
     url = 'invalid_url'
     filename = 'invalid'
     with self.assertRaises(Exception):
       screenshotter.take_screenshot(url, filename)
 
   def test_take_screenshot_with_invalid_filename(self):
-    screenshotter = Screenshotter()
+    screenshotter = Screenshotter(output_dir='test_images')
     url = 'https://www.google.com'
     filename = ''
     with self.assertRaises(Exception):
@@ -29,24 +29,24 @@ class TestScreenshotter(unittest.TestCase):
 
   def test_take_screenshot_with_fullscreen(self):
     img_format = 'png'
-    screenshotter = Screenshotter(fullscreen=True, format=img_format)
+    screenshotter = Screenshotter(fullscreen=True, format=img_format, output_dir='test_images')
     url = 'https://www.google.com'
     filename = 'google_fullscreen'
     screenshotter.take_screenshot(url, filename)
-    self.assertTrue(os.path.exists(filename + '.' + img_format))
+    self.assertTrue(os.path.exists(os.path.join('test_images', filename + '.' + img_format)))
 
   def test_take_screenshot_with_close_popups(self):
     img_format = 'png'
-    screenshotter = Screenshotter(close_popups=True, format=img_format)
+    screenshotter = Screenshotter(close_popups=True, format=img_format, output_dir='test_images')
     url = 'https://www.google.com'
     filename = 'google_close_popups'
     screenshotter.take_screenshot(url, filename)
-    self.assertTrue(os.path.exists(filename + '.' + img_format))
+    self.assertTrue(os.path.exists(os.path.join('test_images', filename + '.' + img_format)))
 
   def test_take_screenshot_with_scroll_page(self):
     img_format = 'png'
-    screenshotter = Screenshotter(scroll_page=True, format=img_format)
+    screenshotter = Screenshotter(scroll_page=True, format=img_format, output_dir='test_images')
     url = 'https://www.google.com'
     filename = 'google_scroll_page'
     screenshotter.take_screenshot(url, filename)
-    self.assertTrue(os.path.exists(filename + '.' + img_format))
+    self.assertTrue(os.path.exists(os.path.join('test_images', filename + '.' + img_format)))
